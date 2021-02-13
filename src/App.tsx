f343adf515635b0b32ee6228c1e94fd5616dd301
petevdp <pjvanderpol@gmail.com>
2021-02-13 09:49:56 -0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {Liquid} from 'liquidjs'
 import logo from './logo.svg';
+import AceEditor from 'react-ace';
 import './App.css';
 
+import "ace-builds/src-noconflict/mode-liquid";
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/theme-github";
+
+
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [templateText, setTemplateText] = useState("");
+  const [output, setOutput] = useState('');
+  
+  useEffect(() => {
+    console.log('evaluating...');
+    evaluateTemplate(inputText, templateText).then((out) => {
+      setOutput(out)
+      console.log(out);
+    });
+  }, [templateText, inputText]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AceEditor mode="json" theme="github" value={inputText} onChange={(value: string) => setInputText(value)}></AceEditor>
+      <AceEditor mode="liquid" theme="github" value={templateText} onChange={(value: string) => setTemplateText(value)}></AceEditor>
+      <div className="output-display">{output}</div>
+      <div className='status'>{}</div>
     </div>
   );
+}
+
+
+async function evaluateTemplate(inputText: string, templateText: string): Promise<string> {
+  const liquid = new Liquid();
+  try {
+    const vars = JSON.parse(inputText);
+    return await liquid.parseAndRender(templateText, vars);
+  } catch (err) {
+    return err.toString();
+  }
+  
 }
 
 export default App;
